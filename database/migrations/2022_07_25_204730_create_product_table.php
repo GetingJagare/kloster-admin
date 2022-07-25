7285d7fbd550f981a63fe03d1789d679037f47eb
GetingJagare <getingjagare@gmail.com>
2022-07-25 22:55:24 +0200

    migrations, entities, layout

---
+++ database/migrations/2022_07_25_204730_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->text('content');
+            $table->float('price')->nullable(false);
+            $table->string('image_url');
+            $table->boolean('is_published')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
